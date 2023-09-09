300614e5d1e9ae8e1f0c51f9f2b80aa924657db3
mthem <mthem@INDLEYETHU>
2023-09-09 15:05:45 +0200

    Add project files.

---
+++ complex Arithmetics/Form1.Designer.vb
@@ -0,0 +1,49 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        calculateBtn = New Button()
+        SuspendLayout()
+        ' 
+        ' calculateBtn
+        ' 
+        calculateBtn.Location = New Point(287, 140)
+        calculateBtn.Name = "calculateBtn"
+        calculateBtn.Size = New Size(75, 23)
+        calculateBtn.TabIndex = 0
+        calculateBtn.Text = "Calculate"
+        calculateBtn.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(calculateBtn)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents calculateBtn As Button
+End Class
---
+++ complex Arithmetics/Form1.vb
@@ -0,0 +1,32 @@
+﻿Imports System.Windows.Forms.Design
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles calculateBtn.Click
+        Dim Price As Decimal
+        Dim quantity As Integer
+        Dim discount As Integer
+        Dim vat As Decimal
+        Dim Total As Decimal
+
+        Price = 10
+        quantity = 10
+        discount = 2
+        vat = 0.15
+        Total = (Price - discount) * quantity
+
+        MsgBox("the total cost before vat is " & "R" & Total)
+
+
+
+
+
+
+
+
+
+
+
+
+
+    End Sub
+End Class
